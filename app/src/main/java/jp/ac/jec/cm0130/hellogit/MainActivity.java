628382704d0f9d53838fe96664458774171d7176
dailovecode <duboliangvictory@gmail.com>
2021-11-16 12:06:18 +0900

    ok

--- app/src/main/java/jp/ac/jec/cm0130/hellogit/MainActivity.java
+++ app/src/main/java/jp/ac/jec/cm0130/hellogit/MainActivity.java
@@ -3,6 +3,7 @@ package jp.ac.jec.cm0130.hellogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"hello,git", Toast.LENGTH_SHORT).show();
     }
 }
